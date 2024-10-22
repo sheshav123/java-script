0029ac6c836d71993e05dc5714cbb30ab5962436
sheshav anand <sheshavanand12345.sa@gmail.com>
2024-10-22 13:02:43 +0000

    datatypes summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,47 @@
+// primitive 
+
+// 7 types  : String, Number, Boolean, Null, Unbdefined, Int, BigInt
+
+const score = 200 
+const scoreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 678676767676767676n //here n represents the bigint
+
+
+// referance (Nont primetive)
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "nagraj", "doga"]; // arary
+
+//object
+let myObject = {
+
+    name : "sheshav",
+    age : 24,
+}
+
+//functions
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber);
+
+
+
+
+
+
+
